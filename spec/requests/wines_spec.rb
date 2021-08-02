e698c40389b4fbb2cbfb79a078aba7a0a87d2fc7
Mauricio Mora <mauricios.mora@gmail.com>
2021-08-02 18:23:17 -0400

    testcontroladorlisto

---
+++ spec/requests/wines_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
+
+
+RSpec.describe WinesController, type: :controller do
+  describe "GET index" do
+
+    it "returns a successful response" do
+      get :index
+      expect(response).to be_successful
+    end
+
+
+    it "assigns @wines" do
+      wine = Wine.create(name: "name", score: 10)
+      get :index
+      expect(assigns(:wines)).to eq([wine])
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "renders the show template" do
+      get :show
+      expect(response).to render_template("show")
+    end
+  end
+end
